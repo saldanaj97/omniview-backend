15238b5c0ca73f54daefaf5392148f8b8fddefb7
saldanaj97 <saldanaj97@gmail.com>
2025-03-05 15:19:39 -0600

    cors and removed the redirects to templates

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
-from app.api.routes import auth, users, public
+from app.api.routes import auth, public, users
 from app.core.config import SECRET_KEY, TEMPLATES_DIR
 
 # Create FastAPI app
@@ -11,6 +12,15 @@ app = FastAPI(
     title="OmniView Backend",
     description="OmniView Backend API",
     version="0.1.0",
+)
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (CHANGE IN PRODUCTION)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Add session middleware
@@ -25,24 +35,9 @@ app.add_middleware(
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 
 # Include routers
-app.include_router(auth.router, prefix="/auth", tags=["authentication"])
-app.include_router(users.router, prefix="/user", tags=["users"])
-app.include_router(public.router, prefix="/public", tags=["public"])
-
-
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    access_token = request.session.get("access_token")
-    return templates.TemplateResponse(
-        "index.html", {"request": request, "token": access_token}
-    )
-
-
-# Home route that shows login or user info
-@app.get("/user", response_class=HTMLResponse)
-async def root(request: Request):
-    user = request.session.get("user")
-    return templates.TemplateResponse("index.html", {"request": request, "user": user})
+app.include_router(auth.router, prefix="/api/auth", tags=["authentication"])
+app.include_router(users.router, prefix="/api/user", tags=["users"])
+app.include_router(public.router, prefix="/api/public", tags=["public"])
 
 
 if __name__ == "__main__":
