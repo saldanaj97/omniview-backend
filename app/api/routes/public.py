060c03e1698e1970f3ce601202cb470a6c0aeac2
saldanaj97 <saldanaj97@gmail.com>
2025-03-05 18:21:02 -0600

    now properly fetches the token and sends the data to the server

--- app/api/routes/public.py
+++ app/api/routes/public.py
@@ -1,12 +1,24 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Header, HTTPException, Request
 
 import app.services.public as public
 
 router = APIRouter()
 
 
+def get_token(authorization: str = Header(None)):
+    if not authorization:
+        raise HTTPException(status_code=401, detail="No access token found")
+
+    token_parts = authorization.split()
+    if len(token_parts) != 2 or token_parts[0].lower() != "bearer":
+        raise HTTPException(
+            status_code=401, detail="Invalid authorization header format"
+        )
+
+    return token_parts[1]
+
+
 @router.get("/top-streams")
-async def get_popular_streams(request: Request):
-    access_token = request.session["access_token"]
-    popular_streams = await public.get_top_streams(access_token=access_token)
-    return {"popular_streams": popular_streams}
+async def get_popular_streams(token: str = Depends(get_token)):
+    popular_streams = await public.get_top_streams(access_token=token)
+    return popular_streams["data"]
